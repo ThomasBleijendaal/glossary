30cd7deed7eda3e7a7b4f9240b03200605247290
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2022-11-08 20:52:48 +0100

    Message handler middleware

---
+++ Glossary/MessageHandlerMiddleware/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using MessageHandlerMiddleware;
+
+var handler = new MiddlewareHandler(new IHandlerPolicy[]
+{
+    new TokenPolicy(),
+    new DebugPolicy()
+});
+
+var client = new HttpClient(handler);
+
+await client.SendAsync(new HttpRequestMessage(HttpMethod.Post, "https://httpbin.org/anything")
+{
+    Content = new  StringContent($$"""
+        {
+            "request": "body"
+        }
+        """, Encoding.UTF8, "application/json")
+});
+
